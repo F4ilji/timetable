e10b8e34f88a8a3c97aa1586d5a22771dbccce16
F4ilj <f4ilj@yandex.ru>
2022-12-30 13:08:59 +0500

    cosmetic changes

--- index.php
+++ index.php
@@ -10,7 +10,7 @@ $qb = new QueryBuilder();
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 
 <div class="container">
-    <form action="add.php" method="post" enctype="multipart/form-data">
+    <form class="mb-5" action="add.php" method="post" enctype="multipart/form-data">
 
 
 
